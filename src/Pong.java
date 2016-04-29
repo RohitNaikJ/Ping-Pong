e02a35aa0d9e0a41997dabe0c9714628d1857b2e
Hrushikeshaxlr8r <rajahrushi@gmail.com>
2016-04-30 03:24:34 +0530

    multiplayer

--- src/Pong.java
+++ src/Pong.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 import java.net.*;
 
 import javax.swing.Timer;
@@ -22,10 +23,11 @@ public class Pong implements ActionListener, KeyListener{
 	public Ball ball;
 	public int difficulty = 1;
 	DatagramSocket socket;
+	public  InetAddress host;
 	
 	public boolean bot = false, w = false, s = false, up = false, down = false;
 	
-	public Pong(){
+	public Pong() throws SocketException, UnknownHostException{
 		Timer timer = new Timer(20, this);
 		JFrame jFrame = new JFrame("Pong");
 		renderer = new Renderer();
@@ -41,14 +43,15 @@ public class Pong implements ActionListener, KeyListener{
 		timer.start(); 
 	}
 
-	private void start() throws SocketException {
-		socket = new DatagramSocket(6543);
+	private void start() throws SocketException, UnknownHostException {
+		socket = new DatagramSocket(6599);
+		host = InetAddress.getByName("10.192.57.238");
 		p1 = new Paddle(this, 1);
 		p2 = new Paddle(this, 2);
 		ball = new Ball(this);
 	}
 
-	public void update(){
+	public void update() throws IOException{
 		ball.update(p1, p2, pong);
 		if(bot) {
 			p1.update(pong,difficulty,ball);
@@ -64,7 +67,7 @@ public class Pong implements ActionListener, KeyListener{
 				SendData = t.getBytes();
 				
 				DatagramPacket Sendpacket = new DatagramPacket(SendData,SendData.length,host,6543);
-				socket.send(SendPacket);
+				socket.send(Sendpacket);
 			}	
 			else if(down)
 			{	p2.move(false);
@@ -73,7 +76,7 @@ public class Pong implements ActionListener, KeyListener{
 				SendData = t.getBytes();
 				
 				DatagramPacket Sendpacket = new DatagramPacket(SendData,SendData.length,host,6543);
-				socket.send(SendPacket);
+				socket.send(Sendpacket);
 			}
 			byte[] ReceiveData = new byte[256];
 			DatagramPacket ReceivePacket = new DatagramPacket(ReceiveData,ReceiveData.length);
@@ -92,10 +95,6 @@ public class Pong implements ActionListener, KeyListener{
 		else if(s)
 			p1.move(false);
 		*/
-		if(up)
-			p2.move(true);
-		else if(down)
-			p2.move(false);
 	}
 	
 	public void render(Graphics2D g) {
@@ -113,7 +112,12 @@ public class Pong implements ActionListener, KeyListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		update();
+		try {
+			update();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		renderer.repaint(); 
 	}
 
@@ -169,7 +173,7 @@ public class Pong implements ActionListener, KeyListener{
 		}
 	}
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws SocketException, UnknownHostException{
 		pong = new Pong();
 	}
 }
